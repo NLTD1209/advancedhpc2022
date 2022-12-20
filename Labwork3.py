0010e8bc000d9f8b5468900f9b2ce1dd953461a1
NLTD1209 <sunbimbim@gmail.com>
2022-12-20 11:48:58 +0000

    Labwork3: GPU greyscale code

--- Labwork3.py
+++ Labwork3.py
@@ -2,6 +2,8 @@ from numba import cuda
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+import math
 
 img = Image.open('000054.JPG')
 img.save('sample.png')
@@ -12,7 +14,6 @@ PixCount = numpy_img.shape[0]* numpy_img.shape[1]
 
 
 numpy_img = numpy_img.reshape(PixCount,3)
-print(numpy_img[500000])
 
 
 def cpu_greyscale(img):
@@ -24,10 +25,37 @@ def cpu_greyscale(img):
     pixel = pixel /3
     new_img[i] = pixel
   return(new_img)
+time1 = time.time()
 greyscale_img = cpu_greyscale(numpy_img)
-print(greyscale_img[500000])
+print('CPU time in seconds: ', time.time()-time1)
+
 
 greyscale_img = greyscale_img.reshape(2642,3930)
 output_img = Image.fromarray(greyscale_img)
+plt.imshow(greyscale_img, cmap='gray', vmin=0, vmax=255)
+plt.show()
+
+
+
+@cuda.jit
+def GPU_grayscale(src, dst):
+    tidx = cuda.threadIdx.x + cuda.blockIdx.x * cuda.blockDim.x
+    g = (src[tidx, 0] + src[tidx, 1] + src[tidx, 2])/3
+    dst[tidx] = g
+
+blocksize = 64
+blocksize_range = [16,32,64,128,256,512,1024]
+devOutput = cuda.device_array((PixCount), np.float64)
+devInput = cuda.to_device(numpy_img)
+
+for blocksize in blocksize_range:
+  gridSize = math.ceil(PixCount / blocksize)
+  time1 = time.time()
+  GPU_grayscale[gridSize, blocksize](devInput, devOutput)
+  print('GPU time in seconds of blocksize ',blocksize,' : ', time.time()-time1)
+
+hostOutput = devOutput.copy_to_host()
+
+greyscale_img = hostOutput.reshape(2642,3930)
 plt.imshow(greyscale_img, cmap='gray', vmin=0, vmax=255)
 plt.show()
